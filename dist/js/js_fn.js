4f396e17b8d67e3ec07e524f75e4171ad18336b8
gary <qwerty10218@gmail.com>
2023-03-19 11:43:33 +0800

    js常數funcation

---
+++ dist/js/js_fn.js
@@ -0,0 +1,11 @@
+// function add(num1, num2) {
+//     return num1 + num2;
+//     console.log('Hello');
+// }
+
+const add = function (num1, num2) {
+    return num1 + num2;
+}
+
+let calc = add(1, 10);
+console.log(cal c);
